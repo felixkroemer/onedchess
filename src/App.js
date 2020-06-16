0c10ff022b5ecffa8d5bb17facd1331e4969cbf3
Felix Kroemer <fkroemer@mail.uni-mannheim.de>
2020-06-16 19:04:17 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div id="App">
+    <div id="upper">
+      <div id="field">
+      </div>
+    </div>
+    <div id="lower">
+    </div>
+  </div>
+  );
+}
+
+export default App;
